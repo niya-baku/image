5b9e3f617f39277dc611d2855acc0eacf69ad4fa
baku_nishiya <niya-baku@users.noreply.github.com>
2018-08-09 02:17:01 +0900

    コントローラーの定義

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,2 +1,16 @@
 class PrototypesController < ApplicationController
+  def new
+    @prototype = Prototype.new(create_params)
+    3.times { @prototype.thumbnails.build }
+  end
+
+  def create
+    @prototype = Prototype.create(create_params)
+    redirect_to '/'
+  end
+
+  private
+    def create_params
+      params.require(:prototype).permit(thumbnails_attributes: [:image])
+    end
 end
