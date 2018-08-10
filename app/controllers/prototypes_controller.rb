df4467facf792ba521ba25e3fe6ffca577b2b92f
baku_nishiya <niya-baku@users.noreply.github.com>
2018-08-10 20:56:26 +0900

    アップロード機能を修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,21 @@
 class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
-    3.times { @prototype.thumbnails.build }
+    4.times { @prototype.thumbnails.build }
   end
 
   def create
     @prototype = Prototype.create(create_params)
-    redirect_to '/'
+
+    respond_to do |format|
+    if @prototype.save
+      format.html { redirect_to @prototype, notice: 'Prototype was successfully created.' }
+      format.json { render :show, status: :created, location: @prototype }
+    else
+      format.html { render :new }
+      format.json { render json: @prototype.errors, status: :unprocessable_entity }
+    end
+  end
   end
 
   private
